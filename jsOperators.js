59f7e69864149c7a017b45e24c15764817214e7a
Dzianis <>
2023-06-02 15:48:17 -0400

    First

---
+++ jsOperators.js
@@ -0,0 +1,25 @@
+'use strict';
+
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
+
+let incr = 10,
+	decr = 5;
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2+2*2 !== '6');
+
+// && и
+// || или
+
+const isChecked = true,
+	isClose = false;
+
+console.log(isChecked || !isClose);
+
---
+++ user-interaction.js
@@ -0,0 +1,17 @@
+'use strict';
+// Модальные окна для пользователя
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Are you 18?', '18');
+// console.log(typeof(answer));
+
+const answers = [];
+// answers[0] = prompt('Your name?', '');
+// answers[1] = prompt('Your last name?', '');
+// answers[2] = prompt('Your age?', '');
+
+console.log(typeof(answers));
+
